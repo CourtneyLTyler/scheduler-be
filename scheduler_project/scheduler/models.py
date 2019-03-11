0bc16c103b5f39ee8be148792e30fbdd50c4888f
Roger Campbell II <rc1336@gmail.com>
2019-03-11 15:00:03 -0400

    completed Unavailability model. Next Step: Build out WeeklyAvailability.

--- scheduler_project/scheduler/models.py
+++ scheduler_project/scheduler/models.py
@@ -3,10 +3,22 @@ from django.db import models
 
 class ScheduleByDay(models.Model):
     date = models.DateField()
-    dayOfWeek: models.CharField(max_length=100)
-    amStaffNum: models.IntegerField()
-    aftStaffNum: models.IntegerField()
-    pmStaffNum: models.IntegerField()
-    amSecNum: models.IntegerField()
-    aftSecNum: models.IntegerField()
-    pmSecNum: models.IntegerField()
+    dayOfWeek = models.CharField(max_length=100)
+    amStaffNum = models.IntegerField()
+    aftStaffNum = models.IntegerField()
+    pmStaffNum = models.IntegerField()
+    amSecNum = models.IntegerField()
+    aftSecNum = models.IntegerField()
+    pmSecNum = models.IntegerField()
+
+
+class Unavailability(models.Model):
+    date = models.DateField()
+    employee = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, related_name='employee')
+    am = models.CharField(max_length=20, required=True, choices=set(
+        ["I am available", "I am not available"]))
+    aft = models.CharField(max_length=20, required=True, choices=set(
+        ["I am available", "I am not available"]))
+    pm = models.CharField(max_length=20, required=True, choices=set(
+        ["I am available", "I am not available"]))
