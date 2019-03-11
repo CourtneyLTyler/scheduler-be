8ea85bd8946ee87d017900e76b03255d850b591c
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-11 16:02:48 -0400

    register models in admin file

--- scheduler_project/scheduler/admin.py
+++ scheduler_project/scheduler/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Employee, Manager, FloorPlan, Section, ScheduleByDay, Unavailability, WeeklyAvailability
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Manager)
+admin.site.register(FloorPlan)
+admin.site.register(Section)
+admin.site.register(ScheduleByDay)
+admin.site.register(Unavailability)
+admin.site.register(WeeklyAvailability)
+
+
