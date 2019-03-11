bff71303df0e20808ffff26afe1b3fef2a85e066
Roger Campbell II <rc1336@gmail.com>
2019-03-11 15:26:49 -0400

    fixed naming conventions.

--- scheduler_project/scheduler/models.py
+++ scheduler_project/scheduler/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class ScheduleByDay(models.Model):
     date = models.DateField()
-    dayOfWeek = models.CharField(max_length=100)
-    amStaffNum = models.IntegerField()
-    aftStaffNum = models.IntegerField()
-    pmStaffNum = models.IntegerField()
-    amSecNum = models.IntegerField()
-    aftSecNum = models.IntegerField()
-    pmSecNum = models.IntegerField()
+    day_of_week = models.CharField(max_length=100)
+    am_staff_num = models.IntegerField()
+    aft_staff_num = models.IntegerField()
+    pm_staff_num = models.IntegerField()
+    am_sec_num = models.IntegerField()
+    aft_sec_num = models.IntegerField()
+    pm_sec_num = models.IntegerField()
 
     def __str__(self):
         return self.date
@@ -18,7 +18,7 @@ class ScheduleByDay(models.Model):
 class Unavailability(models.Model):
     date = models.DateField()
     employee = models.ForeignKey(
-        Employee, on_delete=models.CASCADE, related_name='employee')
+        Employee, on_delete=models.CASCADE, related_name='unavailability')
     am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
     aft = models.CharField(max_length=20, required=True, choices=set(
@@ -32,48 +32,48 @@ class Unavailability(models.Model):
 
 class WeeklyAvailability(models.Model):
     employee = models.ForeignKey(
-        Employee, on_delete=models.CASCADE, related_name='employee')
-    monAm = models.CharField(max_length=20, required=True, choices=set(
+        Employee, on_delete=models.CASCADE, related_name='weekly_availability')
+    mon_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    monAft = models.CharField(max_length=20, required=True, choices=set(
+    mon_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    monPM = models.CharField(max_length=20, required=True, choices=set(
+    mon_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    tueAm = models.CharField(max_length=20, required=True, choices=set(
+    tue_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    tueAft = models.CharField(max_length=20, required=True, choices=set(
+    tue_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    tuePm = models.CharField(max_length=20, required=True, choices=set(
+    tue_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    wedAm = models.CharField(max_length=20, required=True, choices=set(
+    wed_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    wedAft = models.CharField(max_length=20, required=True, choices=set(
+    wed_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    wedPm = models.CharField(max_length=20, required=True, choices=set(
+    wed_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    thuAm = models.CharField(max_length=20, required=True, choices=set(
+    thu_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    thuAft = models.CharField(max_length=20, required=True, choices=set(
+    thu_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    thuPm = models.CharField(max_length=20, required=True, choices=set(
+    thu_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    friAm = models.CharField(max_length=20, required=True, choices=set(
+    fri_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    friAft = models.CharField(max_length=20, required=True, choices=set(
+    fri_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    friPm = models.CharField(max_length=20, required=True, choices=set(
+    fri_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    satAm = models.CharField(max_length=20, required=True, choices=set(
+    sat_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    satAft = models.CharField(max_length=20, required=True, choices=set(
+    sat_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    satPm = models.CharField(max_length=20, required=True, choices=set(
+    sat_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    sunAm = models.CharField(max_length=20, required=True, choices=set(
+    sun_am = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    sunAft = models.CharField(max_length=20, required=True, choices=set(
+    sun_aft = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
-    sunPm = models.CharField(max_length=20, required=True, choices=set(
+    sun_pm = models.CharField(max_length=20, required=True, choices=set(
         ["I am available", "I am not available"]))
 
     def __str__(self):
