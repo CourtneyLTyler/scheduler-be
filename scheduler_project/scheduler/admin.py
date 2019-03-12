11372380b54933e114e9e25fda4a1becb35cfca0
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-12 10:39:41 -0400

    remove pycache files

--- scheduler_project/scheduler/admin.py
+++ scheduler_project/scheduler/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Employee, Manager, Section, ScheduleByDay, Unavailability, WeeklyAvailability
+from .models import Employee, Manager, Section, ScheduleByShift, Unavailability, WeeklyAvailability
 
 admin.site.register(Employee)
 admin.site.register(Manager)
 admin.site.register(Section)
-admin.site.register(ScheduleByDay)
+admin.site.register(ScheduleByShift)
 admin.site.register(Unavailability)
 admin.site.register(WeeklyAvailability)
 
