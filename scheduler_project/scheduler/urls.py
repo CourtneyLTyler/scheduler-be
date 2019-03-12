bc86321f587837c8f2b2e52d0efe843eef802672
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-12 18:17:00 -0400

    add detail update delete to api

--- scheduler_project/scheduler/urls.py
+++ scheduler_project/scheduler/urls.py
@@ -1,28 +1,28 @@
 from django.conf.urls import url
+from django.urls import path
 
-from rest_framework.routers import DefaultRouter
+# from rest_framework.routers import DefaultRouter
 
 from . import views
 
 urlpatterns = [
-    url('employees', views.EmployeeList.as_view(), name='employee-list'),
-    url('managers', views.ManagerList.as_view(), name='manager-list'),
-    url('sections', views.SectionList.as_view(), name='section-list'),
-    url('schedulebyshifts', views.ScheduleByShiftList.as_view(),
-        name='schedule-by-shift-list'),
-    url('unavailability', views.UnavailabilityList.as_view(),
-        name='unavailability-list'),
-    url('weeklyavailability', views.WeeklyAvailabilityList.as_view(),
-        name='weeklyavailability-list'),
-    # url('employee/<int:pk>', views.EmployeeDetail.as_view(), name='employee-detail'),
+    path('employees', views.EmployeeList.as_view(), name='employee-list'),
+    path('employee/<int:pk>', views.EmployeeDetail.as_view(), name='employee-detail'),
+
+    path('managers', views.ManagerList.as_view(), name='manager-list'),
+    path('manager/<int:pk>', views.ManagerDetail.as_view(), name='manager-detail'),
+
+    path('sections', views.SectionList.as_view(), name='section-list'),
+    path('section/<int:pk>', views.SectionDetail.as_view(), name='section-detail'),
+
+    path('schedulebyshifts', views.ScheduleByShiftList.as_view(), name='schedule-by-shift-list'),
+    path('schedulebyshift/<int:pk>', views.ScheduleByShiftDetail.as_view(), name='schedule-by-shift-detail'),
+
+    path('unavailability', views.UnavailabilityList.as_view(),
+    name='unavailability-list'),
+    path('unavailability/<int:pk>', views.UnavailabilityDetail.as_view(), name='unavailability-detail'),
+
+    path('weeklyavailability', views.WeeklyAvailabilityList.as_view(),
+    name='weeklyavailability-list'),
+    path('weeklyavailability/<int:pk>', views.WeeklyAvailabilityDetail.as_view(), name='weeklyavailability-detail'),
 ]
-
-# # todos/urls.py
-# from django.urls import path
-
-# from . import views
-
-# urlpatterns = [
-#     path('', views.EmployeeList.as_view()),
-#     # path('<int:pk>/', views.DetailEmployee.as_view()),
-# ]
--- scheduler_project/scheduler/views.py
+++ scheduler_project/scheduler/views.py
@@ -8,8 +8,16 @@ class EmployeeList(generics.ListCreateAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
 
 class ManagerList(generics.ListCreateAPIView):
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
+
+class ManagerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Manager.objects.all()
     serializer_class = ManagerSerializer
 
@@ -18,8 +26,16 @@ class SectionList(generics.ListCreateAPIView):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
 
+class SectionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+
 
 class ScheduleByShiftList(generics.ListCreateAPIView):
+    queryset = ScheduleByShift.objects.all()
+    serializer_class = ScheduleByShiftSerializer
+
+class ScheduleByShiftDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ScheduleByShift.objects.all()
     serializer_class = ScheduleByShiftSerializer
 
@@ -28,7 +44,15 @@ class UnavailabilityList(generics.ListCreateAPIView):
     queryset = Unavailability.objects.all()
     serializer_class = UnavailabilitySerializer
 
+class UnavailabilityDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Unavailability.objects.all()
+    serializer_class = UnavailabilitySerializer
+
 
 class WeeklyAvailabilityList(generics.ListCreateAPIView):
+    queryset = WeeklyAvailability.objects.all()
+    serializer_class = WeeklyAvailabilitySerializer
+
+class WeeklyAvailabilityDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = WeeklyAvailability.objects.all()
     serializer_class = WeeklyAvailabilitySerializer
