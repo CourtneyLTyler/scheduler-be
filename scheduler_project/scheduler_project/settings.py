b9482b83c1ff9243be1870c68c0f36ac6ffaab90
Roger Campbell II <rc1336@gmail.com>
2019-03-12 16:46:53 -0400

    Finally have the front and backends talking to each other. WAHHHOOO

--- scheduler_project/scheduler_project/settings.py
+++ scheduler_project/scheduler_project/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'scheduler',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +132,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'
+)
