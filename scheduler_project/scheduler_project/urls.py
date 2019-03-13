838efa7b6ec88a8c5ed72137580890a06346021a
Courtney Tyler <courtneytyler7@gmail.com>
2019-03-13 15:20:10 -0400

    add frontend folder, babel, webpack

--- scheduler_project/scheduler_project/urls.py
+++ scheduler_project/scheduler_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('scheduler.urls')),
+    path('', include('frontend.urls')),
 ]
