0779d163e10fdfbee5013d00a9e6ffc1eb113a65
Roger Campbell II <rc1336@gmail.com>
2019-03-13 17:00:00 -0400

    Burned all to the ground. Removed all React instances in the backend.

--- scheduler_project/scheduler/serializers.py
+++ scheduler_project/scheduler/serializers.py
@@ -3,42 +3,6 @@ from .models import Employee, Manager, Section, ScheduleByShift, Unavailability,
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-    # section_red = serializers.HyperlinkedRelatedField(
-    #     view_name='section_red_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-    # section_orange = serializers.HyperlinkedRelatedField(
-    #     view_name='section_orange_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-    # section_yellow = serializers.HyperlinkedRelatedField(
-    #     view_name='section_yellow_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-    # section_green = serializers.HyperlinkedRelatedField(
-    #     view_name='section_green_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-    # section_blue = serializers.HyperlinkedRelatedField(
-    #     view_name='section_blue_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-    # section_purple = serializers.HyperlinkedRelatedField(
-    #     view_name='section_purple_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-    # employee = serializers.HyperlinkedRelatedField(
-    #     view_name='employee_detail',
-    #     many=True,
-    #     read_only=True
-    # )
-
     class Meta:
         model = Employee
         fields = ('id', 'full_name', 'position',
