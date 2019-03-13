5d1cf454f34e7fc438732c88139249de336832d3
Roger Campbell II <rc1336@gmail.com>
2019-03-13 15:22:01 -0400

    Just a commit before I move over to the combine branch.

--- scheduler_project/scheduler_project/settings.py
+++ scheduler_project/scheduler_project/settings.py
@@ -129,7 +129,8 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny',
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
 
